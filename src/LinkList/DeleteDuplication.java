ce3a2230ef2e43379b81d48f1a83fe60dee204a2
zakree <957681484@qq.com>
2019-06-29 16:50:33 +0800

    deleteDuplication

--- src/LinkList/DeleteDuplication.java
+++ src/LinkList/DeleteDuplication.java
@@ -1,4 +1,39 @@
 package LinkList;
 
-public class DeleteDuplication {
-}
+
+
+    public class DeleteDuplication {
+         class ListNode {
+             int val;
+             ListNode next = null;
+
+             ListNode(int val) {
+                 this.val = val;
+             }
+         }
+
+        public ListNode deleteDuplication(ListNode pHead) {
+            if (pHead == null || pHead.next == null) {
+                return pHead;
+            }
+            ListNode newH = new ListNode(0);
+            newH.next = pHead;
+            ListNode p = newH;
+            ListNode n = newH.next;
+
+            while (n != null) {
+                if (n.next != null && n.val == n.next.val) {
+                    while (n.next != null && n.val == n.next.val) {
+                        n = n.next;//如果有多个重复 p不用动 n动到最后一个重复在删除
+                    }
+                    p.next = n.next;
+                    n = n.next;
+                } else {
+                    p = p.next;
+                    n = n.next;
+                }
+            }
+            return newH.next;
+        }
+
+    }
