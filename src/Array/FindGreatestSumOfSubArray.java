8cde10ac985e12a5f7739398fa14d642a8584be8
zakree <957681484@qq.com>
2019-06-30 23:23:05 +0800

    FindGreatestSumOfSubArray2

--- src/Array/FindGreatestSumOfSubArray.java
+++ src/Array/FindGreatestSumOfSubArray.java
@@ -21,4 +21,17 @@ public class FindGreatestSumOfSubArray {
         }
         return max;
     }
+
+    /**
+     *动态规划
+     */
+    public  int FindGreatestSumOfSubArray2(int[] array) {
+        int res = array[0]; //记录当前所有子数组的和的最大值
+        int max=array[0];   //包含array[i]的连续数组最大值
+        for (int i = 1; i < array.length; i++) {
+            max=Math.max(max+array[i], array[i]);
+            res=Math.max(max, res);
+        }
+        return res;
+    }
 }
