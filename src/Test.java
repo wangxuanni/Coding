9fc36e65cbad490053c54e5d0174088cfa61cb71
zakree <957681484@qq.com>
2019-07-02 22:34:10 +0800

    TreeNode

---
+++ src/Test.java
@@ -0,0 +1,7 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Test {
+    ArrayList A=new ArrayList();
+
+}
---
+++ src/tree/TreeNode.java
@@ -0,0 +1,12 @@
+package tree;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+
+}
