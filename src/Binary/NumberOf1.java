8ead831287933980b72c4f523631b459e4d09a8b
zakree <957681484@qq.com>
2019-07-06 17:56:17 +0800

    NumberOf1

---
+++ src/Binary/NumberOf1.java
@@ -0,0 +1,12 @@
+package Binary;
+
+public class NumberOf1 {
+    public int NumberOf1(int n) {
+        int count=0;
+        while(n!=0){
+            count++;
+            n=(n-1)&n;
+        }
+        return count;
+    }
+}
