36f2c6b92671c5e9027b89d23991df784fba9dfb
zakree <957681484@qq.com>
2019-07-07 21:16:25 +0800

    Fibonacci

---
+++ src/Other/Fibonacci.java
@@ -0,0 +1,10 @@
+package Other;
+
+public class Fibonacci {
+    public int Fibonacci(int n) {
+        if(n==1||n==0){
+            return n;
+        }
+        return Fibonacci(n-1)+Fibonacci(n-2);
+    }
+}
