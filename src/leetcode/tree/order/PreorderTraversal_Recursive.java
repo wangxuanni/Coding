b8514f2fd591b2c7940d0d379ca76d4168c509c4
zakree <957681484@qq.com>
2019-09-18 23:54:32 +0800

    PreorderTraversal_Recursive

---
+++ src/leetcode/tree/order/PreorderTraversal_Recursive.java
@@ -0,0 +1,35 @@
+package leetcode.tree.order;
+
+import tree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * @description:
+ * @author: wangxuanni
+ * @create: 2019-09-18 23:53
+ **/
+
+public class PreorderTraversal_Recursive {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+        Stack <TreeNode> stack = new Stack<>();
+
+        if (root==null)return list;
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node=stack.pop();
+            if (node.right!=null)stack.push(node.right);
+
+            if (node.left!=null) stack.push(node.left);
+
+            list.add(node.val);
+
+        }
+
+        return list;
+    }
+
+}
