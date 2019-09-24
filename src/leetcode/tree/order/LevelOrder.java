512056963541bc85861b001644165ba94a3f9c7c
zakree <957681484@qq.com>
2019-09-24 09:53:24 +0800

    levelOrder

---
+++ src/leetcode/tree/order/LevelOrder.java
@@ -0,0 +1,43 @@
+package leetcode.tree.order;
+
+import tree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @description:
+ * @author: wangxuanni
+ * @create: 2019-09-24 09:52
+ **/
+
+public class LevelOrder {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (root == null) return result;
+
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int cur = queue.size();
+            List<Integer> list = new ArrayList<>();
+            while (cur > 0) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+
+                }
+                cur--;
+
+            }
+            result.add(list);
+        }
+        return result;
+    }
+}
