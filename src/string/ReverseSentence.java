ee276b2a670c803f2397530553c6a5e097628e1a
zakree <957681484@qq.com>
2019-09-24 09:57:49 +0800

    ReverseSentence

--- src/string/ReverseSentence.java
+++ src/string/ReverseSentence.java
@@ -1,9 +1,31 @@
 package string;
 
+import java.util.Scanner;
+
 public class ReverseSentence {
     public String ReverseSentence(String str) {
+        char[] str1={'d','f','w'};
+    str=str1.toString();
         if (str.trim().equals("")) {
-            return str;
+        return str;
+    }
+    String[] sarr = str.split(" ");
+    StringBuilder sb = new StringBuilder();
+        for (int i = sarr.length - 1; i >= 0; i--) {
+        if (i == 0) {
+            sb.append(sarr[i]);
+        } else {
+            sb.append(sarr[i] + " ");
+        }
+    }
+        return sb.toString();
+}
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        if (str.trim().equals("")) {
+            System.out.println("");
         }
         String[] sarr = str.split(" ");
         StringBuilder sb = new StringBuilder();
@@ -14,6 +36,6 @@ public class ReverseSentence {
                 sb.append(sarr[i] + " ");
             }
         }
-        return sb.toString();
+        System.out.println(sb.toString());
     }
 }
