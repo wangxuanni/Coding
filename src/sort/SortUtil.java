4c127ec1308a335012767a8ddeaf8e4c166a434b
zakree <957681484@qq.com>
2019-10-20 18:10:26 +0800

    测试排序正确性的工具

---
+++ src/sort/SortUtil.java
@@ -0,0 +1,105 @@
+package sort;
+
+import java.util.Arrays;
+
+/**
+ * @description:测试排序正确性的工具，修改两处注释下方的调用代码，即可以测试自己写的排序类是否正确。
+ * @author: wangxuanni
+ * @create: 2019-10-19 00:23
+ **/
+
+public class SortUtil {
+    // for test
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 10;
+        int maxValue = 100;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = copyArray(arr1);
+            /*
+             * test修改处二
+             */
+            // QuickSort2.quickSort(arr1, 0, arr1.length-1);
+            HeapSort2.heapSort2(arr1);
+
+            comparator(arr2);
+            if (!isEqual(arr1, arr2)) {
+                succeed = false;
+                printArray(arr1);
+                printArray(arr2);
+                break;
+            }
+        }
+        System.out.println(succeed ? "Nice!" : "Fucking fucked!");
+        int[] arr1 = generateRandomArray(maxSize, maxValue);
+        printArray(arr1);
+
+        /**
+         * test修改处二
+         */
+        HeapSort2.heapSort2(arr1);
+        //QuickSort2.quickSort(arr1, 0, arr1.length - 1);
+        printArray(arr1);
+
+    }
+
+    // for test
+    public static void comparator(int[] arr) {
+        Arrays.sort(arr);
+    }
+
+    // for test
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
+    }
+
+    // for test
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = arr[i];
+        }
+        return res;
+    }
+
+    // for test
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // for test
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+
+}
