c6373fc84eeecdaa8c5b5bfb9a8fa16773a3c7a3
zakree <957681484@qq.com>
2019-10-21 19:05:39 +0800

    MaximumSwap

---
+++ src/leetcode/array/MaximumSwap.java
@@ -0,0 +1,23 @@
+package leetcode.array;
+
+/**
+ * @description:给定一个非负整数，你至多可以交换一次数字中的任意两位。返回你能得到的最大值。 示例 1 :
+ * 输入: 2736
+ * 输出: 7236
+ * 解释: 交换数字2和数字7。
+ * 示例 2 :
+ * 输入: 9973
+ * 输出: 9973
+ * 解释: 不需要交换。
+ * @author: wangxuanni
+ * @create: 2019-10-05 16:26
+ **/
+
+public class MaximumSwap {
+    public static void main(String[] args) {
+        System.out.println(maximumSwap(2736));
+    }
+    public static int maximumSwap(int num) {
+        return 1;
+    }
+}
