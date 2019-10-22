dd1aeec9582f1e7229b541850a2fd5700a8ad58c
zakree <957681484@qq.com>
2019-10-22 17:50:08 +0800

    TreeNode

---
+++ src/leetcode/tree/TreeNode.java
@@ -0,0 +1,13 @@
+package leetcode.tree;
+
+public class TreeNode {
+
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+
+}
