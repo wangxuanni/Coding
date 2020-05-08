ef9ae1a8ded3f5adab1542cc23a00f2def86cfbc
zakree <957681484@qq.com>
2020-05-08 23:47:25 +0800

    commit

--- src/leetcode/link/HasCycle.java
+++ src/leetcode/link/HasCycle.java
@@ -8,9 +8,10 @@ package leetcode.link;
 
 public class HasCycle {
     public boolean hasCycle(ListNode head) {
+        if(head==null||head.next==null)return false;
         ListNode slow=head;
         ListNode fast=head;
-        while(fast.next.next!=null){
+        while(fast.next!=null&&fast.next.next!=null){
             slow=slow.next;
             fast=fast.next.next;
             if (slow==fast)return true;
