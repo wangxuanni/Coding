1b799fe033616ab5c982513515590df5150a33d8
zakree <957681484@qq.com>
2020-05-31 22:51:59 +0800

    commit

--- src/leetcode/dp/ClimbingStairs.java
+++ src/leetcode/dp/ClimbingStairs.java
@@ -7,6 +7,35 @@ package leetcode.dp;
  **/
 
 public class ClimbingStairs {
+    public static void main(String[] args) {
+        int n = 3;
+        System.out.println(climb(n));
+
+        int[] memo = new int[n+1];
+        //Arrays.fill(memo,0);
+        System.out.println(climb_dp(n,memo));
+
+
+    }
+
+    static int climb(int n) {
+        if (n==1)
+            return 1;
+        if (n==2)
+            return 2;
+
+        return climb(n-1)+climb(n-2);
+    }
+    static int climb_dp(int n,int []memo) {
+        if (n==1)
+            return 1;
+        if (n==2)
+            return 2;
+        if (memo[n]==0)
+            memo[n]= climb_dp(n-1,memo)+climb_dp(n-2,memo);
+        return memo[n];
+    }
+
     public int f(int n) {
         if (n <= 2) {
             return n;
@@ -14,9 +43,9 @@ public class ClimbingStairs {
         int pre1 = 1;
         int pre2 = 2;
         for (int i = 2; i < n; i++) {
-           int cur = pre1 + pre2;
+            int cur = pre1 + pre2;
             pre1 = pre2;
-            pre2=cur;
+            pre2 = cur;
         }
         return pre2;
     }
