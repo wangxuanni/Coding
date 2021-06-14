f4f0aa4399613d85ec0fa10af776dd54524f0f8c
keyu <957681484@qq.com>
2021-06-14 21:34:07 +0800

    commit

---
+++ src/structure/tree/preorderTraversal_recursion.java
@@ -0,0 +1,33 @@
+package structure.tree;
+
+import leetcode.tree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 递归写法，很简单
+ * @author keyu
+ * @since 2021-06-14 19:47
+ **/
+
+public class preorderTraversal_recursion {
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        ArrayList list = new ArrayList<Integer>();
+        return f(root, list);
+
+    }
+
+    public static List<Integer> f(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return list;
+        }
+
+        list.add(root.val);
+        f(root.left, list);
+        f(root.right, list);
+        return list;
+    }
+
+}
