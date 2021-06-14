3e7e4025c2bb12bc5635a996e81f19c74126b991
keyu <957681484@qq.com>
2021-06-14 22:42:29 +0800

    commit

---
+++ src/structure/tree/PreorderTraversal.java
@@ -0,0 +1,32 @@
+package structure.tree;
+
+import leetcode.tree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * @author keyu
+ * @since 2021-06-14 22:41
+ **/
+
+public class PreorderTraversal {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if(root==null){return new ArrayList(0);}
+        ArrayList result= new ArrayList<Integer>();
+        Stack<TreeNode> stack=new Stack<TreeNode>();
+        stack.push(root);
+        while(!stack.isEmpty()){
+            TreeNode index= stack.pop();
+            result.add(index.val);
+            if(index.right!=null){
+                stack.push(index.right);
+            }
+            if(index.left!=null){
+                stack.push(index.left);
+            }
+        }
+        return result;
+    }
+}
