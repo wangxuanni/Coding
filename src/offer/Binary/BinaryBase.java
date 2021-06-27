e04f4b6f09fdc7ca084672b7f86a191d2eb2023d
keyu <keyu@mogu.com>
2021-06-27 14:54:01 +0800

    commit

---
+++ src/offer/Binary/BinaryBase.java
@@ -0,0 +1,20 @@
+package offer.Binary;
+
+/**
+ * @author keyu
+ * @since 2021-06-27 14:50
+ **/
+
+public class BinaryBase {
+    public static void main(String[] args) {
+        System.out.println(1&1);
+        System.out.println(1&0);
+        System.out.println(0&0);
+        System.out.println(1|1);
+        System.out.println(1|0);
+        System.out.println(0|0);
+        System.out.println(1^1);
+        System.out.println(1^0);
+        System.out.println(0^0);
+    }
+}
