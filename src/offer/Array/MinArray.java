3e01e5b0509b96329a1cf7dd8d81c51cb1ddd3b0
keyu <keyu@mogu.com>
2021-06-27 14:44:14 +0800

    commit

---
+++ src/offer/Array/MinArray.java
@@ -0,0 +1,25 @@
+package offer.Array;
+
+/**
+ * 旋转数组的最小数字
+ * -。-想不明白这道题
+ * @author keyu
+ * @since 2021-06-27 14:37
+ **/
+
+public class MinArray {
+    public static void main(String[] args) {
+        int[] numbers = {2,2,2,0,1};
+        System.out.println(minArray(numbers));
+    }
+
+    public static int minArray(int[] numbers) {
+        int mid = 0;
+        int lIdx = 0;
+        int rIdx = numbers.length - 1;
+        while (lIdx < rIdx) {
+
+        }
+        return numbers[lIdx];
+    }
+}
