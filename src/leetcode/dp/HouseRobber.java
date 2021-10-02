306bd447de9c3ad30417def4a78a205dabf5febf
keyu <keyu@mogu.com>
2021-10-02 21:16:47 +0800

    HouseRobber

---
+++ src/leetcode/dp/HouseRobber.java
@@ -0,0 +1,22 @@
+package leetcode.dp;
+
+/**
+ * @author zakree
+ * @since 2021-10-02 15:38
+ **/
+
+public class HouseRobber {
+    public static void main(String[] args) {
+        int[] arr={2,7,9,3,1};
+        rob(arr);
+    }
+    public static int rob(int[] nums) {
+        int preTwo=0; int preOne=0;
+        for (int i = 0; i < nums.length; i++) {
+            int chooseSumOrCur=Math.max(preTwo+nums[i],preOne);
+            preTwo=preOne;
+            preOne=chooseSumOrCur;
+        }
+        return preOne;
+    }
+}
