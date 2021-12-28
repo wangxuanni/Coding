9ac274e764c3a3560f65445c5114b5ecd821f19b
keyu <957681484@qq.com>
2021-12-28 17:50:45 +0800

    commit

--- src/cracking/IsUnique_Bit.java
+++ src/cracking/IsUnique_Bit.java
@@ -3,31 +3,28 @@ package cracking;
 /**
  * 实现一个算法，确定一个字符串 s 的所有字符是否全都不同。
  * 位运算
+ * 用Integer代替布尔数组
+ * 该坐标会设置为1，如果原本位置是0，则会被设置为1，设置为出现过
+ * 如果原本位置是0，说明之前已经出现过了，则返回false
  *
  * @author keyu
  * @since 2021-12-27 14:26
  **/
 
 public class IsUnique_Bit {
-    public boolean isUnique(String s) {
-        long left = 0;
-        long right = 0;
-        for (char c : s.toCharArray()) {
-            if (c >= 64) {
-                long bitIndex = 1L << (c - 64);
-                if ((left & bitIndex) != 0) {
-                    return false;
-                }
-                left |= bitIndex;
+
+    public boolean isUnique(String astr) {
+        Integer hasOurInt = new Integer(0);
+        boolean[] hasOur = new boolean[26];
+        for (char c : astr.toCharArray()) {
+            int index = c - 'a';
+            int i = index << 1;
+            if ((i & hasOurInt) == 0) {
+                hasOurInt |= i;
             } else {
-                long bitIndex = 1L << c;
-                if ((right & bitIndex) != 0) {
-                    return false;
-                }
-                right |= bitIndex;
+                return false;
             }
         }
         return true;
-
     }
 }
