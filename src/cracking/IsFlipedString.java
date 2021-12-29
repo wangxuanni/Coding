e29036d3f36e4a45bb5a91b40a0cc786fa4c3386
keyu <957681484@qq.com>
2021-12-29 17:37:21 +0800

    isFlipedString

---
+++ src/cracking/IsFlipedString.java
@@ -0,0 +1,14 @@
+package cracking;
+
+/**
+ * 字符串轮转。给定两个字符串s1和s2，请编写代码检查s2是否为s1旋转而成（比如，waterbottle是erbottlewat旋转后的字符串）。
+ * @author keyu
+ * @since 2021-12-29 17:36
+ **/
+
+public class IsFlipedString {
+    public boolean isFlipedString(String s1, String s2) {
+        return s1.length() == s2.length() &&( s1 + s1).contains(s2);
+
+    }
+}
