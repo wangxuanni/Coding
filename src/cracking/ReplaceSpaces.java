c00ad1629efaa4698f1d82316276529e8980cb00
keyu <957681484@qq.com>
2021-12-30 20:55:28 +0800

    ReplaceSpaces

--- src/cracking/ReplaceSpaces.java
+++ src/cracking/ReplaceSpaces.java
@@ -6,25 +6,23 @@ package cracking;
  **/
 
 public class ReplaceSpaces {
-    public static String replaceSpaces(String S, int length) {
-        //先把字符串转化为字符数组
+    public String replaceSpaces(String S, int length) {
+        if (S == null || S.length() == 0)
+            return S;
         char[] chars = S.toCharArray();
-        int index = chars.length - 1;
+        int index = S.length() - 1;
         for (int i = length - 1; i >= 0; i--) {
-            //如果遇到空格就把他转化为"%20"
-            if (chars[i] == ' ') {
+            char c = chars[i];
+            if (c == ' ') {
                 chars[index--] = '0';
                 chars[index--] = '2';
                 chars[index--] = '%';
             } else {
-                chars[index--] = chars[i];
+                chars[index--] = c;
             }
-        }
-        return new String(chars, index + 1, chars.length - index - 1);
-    }
 
-    public static void main(String[] args) {
-        String mr_john_smith____ = replaceSpaces("Mr John Smith    ", 13);
-        System.out.println("result:"+mr_john_smith____);
+        }
+        index++;
+        return new String(chars, index, chars.length - index);
     }
 }
