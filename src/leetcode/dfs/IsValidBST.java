dbda532d1c50d514042e54a70abce34d8db1a2a3
keyu <957681484@qq.com>
2022-01-15 20:33:09 +0800

    IsValidBST

---
+++ src/leetcode/dfs/IsValidBST.java
@@ -0,0 +1,34 @@
+package leetcode.dfs;
+
+import leetcode.tree.TreeNode;
+
+import java.util.ArrayList;
+
+/**
+ * @author keyu
+ * @since 2022-01-14 10:40
+ **/
+
+public class IsValidBST {
+    public boolean isValidBST(TreeNode root) {
+        ArrayList<Integer> list = new ArrayList<>();
+        f(root, list);
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i - 1) >= list.get(i)) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+    void f(TreeNode root, ArrayList list) {
+        if (root == null)
+            return;
+
+        f(root.left, list);
+        list.add(root.val);
+        f(root.right, list);
+    }
+
+}
