c4352b31240ad3b442271c28ee8785c1eef72b83
keyu <957681484@qq.com>
2022-01-15 20:33:21 +0800

    KthLargest

---
+++ src/offer/tree/KthLargest.java
@@ -0,0 +1,28 @@
+package offer.tree;
+
+import java.util.ArrayList;
+
+/**
+ * @author keyu
+ * @since 2022-01-07 18:31
+ **/
+
+public class KthLargest {
+    public int kthLargest(TreeNode root, int k) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+        f(root, k, list);
+        return list.get(list.size() - 1 - k>0?list.size() - 1 - k:list.size() - 1 );
+
+    }
+
+    public void f(TreeNode root, int k, ArrayList<Integer> list) {
+        if (root == null)
+            return;
+
+        list.add(root.val);
+        f(root.left, k,list);
+        f(root.right, k,list);
+    }
+
+}
