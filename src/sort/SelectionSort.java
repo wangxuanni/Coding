f9b46623ac07e790a81faef84a38257a61379031
keyu <957681484@qq.com>
2022-01-25 17:11:02 +0800

    SelectionSort

--- src/sort/SelectionSort.java
+++ src/sort/SelectionSort.java
@@ -6,4 +6,12 @@ package sort;
  **/
 
 public class SelectionSort {
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j])
+                    SortUtil.swap(arr, i, j);
+            }
+        }
+    }
 }
