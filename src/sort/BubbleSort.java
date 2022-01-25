ce6e572794557ddb26024e5a5a1debecae4ad9de
keyu <957681484@qq.com>
2022-01-25 17:11:27 +0800

    BubbleSort

---
+++ src/sort/BubbleSort.java
@@ -0,0 +1,17 @@
+package sort;
+
+/**
+ * @author keyu
+ * @since 2022-01-24 15:15
+ **/
+
+public class BubbleSort {
+    public static void bubbleSort(int[] arr) {
+        for (int i = arr.length - 1; i >= 0; i--) {//i[最大坐标~最小坐标]
+            for (int j = 0; j < i; j++) {//j[最小坐标~i-1]
+                if (arr[j] > arr[j + 1])
+                    SortUtil.swap(arr, j, j + 1);
+            }
+        }
+    }
+}
