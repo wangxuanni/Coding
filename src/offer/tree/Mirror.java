492af8149ee1cf66c00272227ea2355cf20a8601
keyu <957681484@qq.com>
2022-02-23 10:41:04 +0800

    二叉树的镜像

--- src/offer/tree/Mirror.java
+++ src/offer/tree/Mirror.java
@@ -1,14 +1,21 @@
 package offer.tree;
 
+/**
+ * 二叉树的镜像
+ * @author wangxuanni
+ * @since 2022-02-23 10:36
+ **/
 public class Mirror {
-    public void Mirror(TreeNode root) {
-        if(root==null){
-            return;
+    public static void main(String[] args) {
+        mirror(null);
+    }
+    public static TreeNode mirror(TreeNode root) {
+        if (root == null) {
+            return null;
         }
-        TreeNode t=root.left;
-        root.left=root.right;
-        root.right=t;
-        Mirror(root.left);
-        Mirror(root.right);
+        TreeNode temp = root.left;
+        root.left = mirror(root.right);
+        root.right = mirror(temp);
+        return root;
     }
 }
