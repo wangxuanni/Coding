ac1ddcfada3a4eecf019c86b2fbf03f9604b92c9
keyu <957681484@qq.com>
2022-03-16 18:20:22 +0800

    FooBar

---
+++ src/leetcode/thread/FooBar.java
@@ -0,0 +1,39 @@
+package leetcode.thread;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * @author keyu
+ * @since 2022-03-09 15:58
+ **/
+
+public class FooBar {private int n;
+    BlockingQueue<Integer> foo = new ArrayBlockingQueue<Integer>(1);
+    BlockingQueue<Integer> bar = new ArrayBlockingQueue<Integer>(1);
+
+    public FooBar(int n) {
+        this.n = n;
+    }
+
+    public void foo(Runnable printFoo) throws InterruptedException {
+        foo.add(1);
+        for (int i = 0; i < n; i++) {
+            foo.take();
+            // printFoo.run() outputs "foo". Do not change or remove this line.
+            printFoo.run();
+            bar.add(1);
+        }
+
+    }
+
+    public void bar(Runnable printBar) throws InterruptedException {
+
+        for (int i = 0; i < n; i++) {
+            bar.take();
+            // printBar.run() outputs "bar". Do not change or remove this line.
+            printBar.run();
+            foo.add(1);
+        }
+    }
+}
