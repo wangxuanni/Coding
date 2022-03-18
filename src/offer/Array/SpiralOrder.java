b0ec8b65302c390b47c332ca0023857c19e3526b
keyu <957681484@qq.com>
2022-03-18 11:45:18 +0800

    顺时针打印矩阵

--- src/offer/Array/SpiralOrder.java
+++ src/offer/Array/SpiralOrder.java
@@ -1,6 +1,7 @@
 package offer.Array;
 
 /**
+ * 顺时针打印矩阵
  * @author keyu
  * @since 2022-03-11 12:18
  **/
@@ -8,18 +9,42 @@ package offer.Array;
 public class SpiralOrder {
     public int[] spiralOrder(int[][] matrix) {
         if (matrix.length == 0) return new int[0];
-        int xStart = 0, y = matrix[0].length - 1, yStart = 0, x = matrix.length - 1, count = 0;
-        int[] res = new int[(y + 1) * (x + 1)];
+
+        int count = 0, left = 0, top = 0, right = matrix[0].length - 1, bottom = matrix.length - 1;
+        int[] result = new int[(right + 1) * (bottom + 1)];
         while (true) {
-            for (int i = xStart; i <= y; i++) res[count++] = matrix[yStart][i]; // left to right
-            if (++yStart > x) break;
-            for (int i = yStart; i <= x; i++) res[count++] = matrix[i][y]; // top to bottom
-            if (xStart > --y) break;
-            for (int i = y; i >= xStart; i--) res[count++] = matrix[x][i]; // right to left
-            if (yStart > --x) break;
-            for (int i = x; i >= yStart; i--) res[count++] = matrix[i][xStart]; // bottom to top
-            if (++xStart > y) break;
+
+            //left往右移动，top减一，直到top>=bottom
+            for (int i = left; i <= right; i++) {
+                result[count++] = matrix[top][i];
+                //System.out.println(result[count - 1]);
+
+            }
+            if (++top > bottom) break;
+            //top往下移动，right减一，直到right<=left
+            for (int i = top; i <= bottom; i++) {
+                result[count++] = matrix[i][right];
+                //System.out.println(result[count - 1]);
+
+            }
+            if (--right < left) break;
+
+            //right往左移动，bottom加一，直到bottom<=top
+            for (int j = right; j >= left; j--) {
+                result[count++] = matrix[bottom][j];
+                //System.out.println(result[count - 1]);
+
+            }
+            if (--bottom < top) break;
+            //bottom往上移动，left加一，直到left>=right
+            for (int j = bottom; j >= top; j--) {
+                result[count++] = matrix[j][left];
+                //System.out.println(result[count - 1]);
+
+            }
+            if (++left > right) break;
+
         }
-        return res;
+        return result;
     }
 }
