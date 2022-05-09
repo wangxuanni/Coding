c8ca1fe23d7966a531f5330b093a7fac8cc8558d
keyu <957681484@qq.com>
2022-05-09 15:15:27 +0800

    树的子结构

--- src/offer/tree/IsSubStructure.java
+++ src/offer/tree/IsSubStructure.java
@@ -1,45 +1,30 @@
 package offer.tree;
 
 /**
+ * 树的子结构
  * @author wangxuanni
  * @since 2022-01-04 14:16
  **/
 
 public class IsSubStructure {
 
+
     public boolean isSubStructure(TreeNode A, TreeNode B) {
-        if (B == null) {
-            if (A == null) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-        return fTree(A, B);
+        if (A == null) return B == null;
+
+        /*  A:[-1,3,2,0]B:[]
+    测试结果:true
+    期望结果:false*/
+        if (B == null) return false;
+
+        return check(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B);
     }
 
-    boolean fTree(TreeNode currentANode, TreeNode B) {
-        if (currentANode == null) {
-            return false;
-        }
-        boolean a = f(currentANode, B);
-        boolean b = fTree(currentANode.left, B);
-        boolean c = fTree(currentANode.right, B);
-        return a || b || c;
-    }
+    public boolean check(TreeNode A, TreeNode B) {
+        if (B == null) return true;
 
-    public boolean f(TreeNode aNode, TreeNode bNode) {
-        if (bNode == null) {
-            return true;
-        }
-        if (aNode == null) {
-            return false;
-        }
-        if (aNode.val != bNode.val) {
-            return false;
-        }
-        boolean left = f(aNode.left, bNode.left);
-        boolean right = f(aNode.right, bNode.right);
-        return left && right;
+        if (A == null || A.val != B.val) return false;
+
+        return check(A.left, B.left) && check(A.right, B.right);
     }
 }
